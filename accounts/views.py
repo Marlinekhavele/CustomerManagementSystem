925386636eecda8c1b228147a0213aa80560f796
MarlineKhavele <marlinekhavele6@gmail.com>
2020-03-03 09:30:13 +0300

    Added templates

--- accounts/views.py
+++ accounts/views.py
@@ -6,13 +6,13 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse("home")
+    return render(request, "accounts/dashboard.html")
 
 
 def products(request):
-    return HttpResponse("products")
+    return render(request, "accounts/products.html")
 
 
 def customer(request):
-    return HttpResponse("customer")
+    return render(request, "accounts/customer.html")
 
