242d268b45bdf181abb3dcf0fb1c8bdd53e96dcd
MarlineKhavele <marlinekhavele6@gmail.com>
2020-03-03 22:12:50 +0300

    Added CRUD on views

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import *
+from .forms import OrderForm
 
 # Create your views here.
 
@@ -37,4 +38,38 @@ def customer(request, pk_test):
         "order_count": order_count,
     }
     return render(request, "accounts/customer.html", context)
+
+
+def createOrder(request):
+    form = OrderForm()
+    context = {"form": form}
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    return render(request, "accounts/order_form.html", context)
+
+
+def UpdateOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    form = OrderForm(instance=order)
+    if request.method == "POST":
+        form = OrderForm(request.POST, instance=order)
+        if form.is_valid():
+            form.save()
+            return redirect("/")
+
+    context = {"form": form}
+    return render(request, "accounts/order_form.html", context)
+
+
+def deleteOrder(request, pk):
+    order = Order.objects.get(id=pk)
+    if request.method == "POST":
+        order.delete()
+        return redirect("/")
+    context = {"item": order}
+    return render(request, "accounts/delete.html", context)
 
