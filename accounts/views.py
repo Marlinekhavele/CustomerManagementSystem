9cfaca0a60c4a1642c25b4e1bb5448de42fc416b
MarlineKhavele <marlinekhavele6@gmail.com>
2020-03-04 08:39:47 +0300

    Added filter form

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.forms import inlineformset_factory
 from .models import *
 from .forms import OrderForm
+from .filters import OrderFilter
 
 # Create your views here.
 
@@ -12,7 +14,7 @@ def home(request):
     total_customers = customers.count()
     total_orders = orders.count()
     delivered = orders.filter(status="Delivered").count()
-    pending = orders.filter(status="pending").count()
+    pending = orders.filter(status="Pending").count()
     context = {
         "orders": orders,
         "customers": customers,
@@ -32,23 +34,33 @@ def customer(request, pk_test):
     customer = Customer.objects.get(id=pk_test)
     orders = customer.order_set.all()
     order_count = orders.count()
+    myFilter = OrderFilter(request.GET, queryset=orders)
+    orders = myFilter.qs
     context = {
         "customer": customer,
         "orders": orders,
         "order_count": order_count,
+        "myFilter": myFilter,
     }
     return render(request, "accounts/customer.html", context)
 
 
-def createOrder(request):
-    form = OrderForm()
-    context = {"form": form}
+def createOrder(request, pk):
+    OrderFormSet = inlineformset_factory(
+        Customer, Order, fields=("product", "status"), extra=10
+    )
+    customer = Customer.objects.get(id=pk)
+    formset = OrderFormSet(queryset=Order.objects.none(), instance=customer)
+    # form = OrderForm(initial={'customer':customer})
     if request.method == "POST":
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            form.save()
+        # print('Printing POST:', request.POST)
+        # form = OrderForm(request.POST)
+        formset = OrderFormSet(request.POST, instance=customer)
+        if formset.is_valid():
+            formset.save()
             return redirect("/")
 
+    context = {"form": formset}
     return render(request, "accounts/order_form.html", context)
 
 
