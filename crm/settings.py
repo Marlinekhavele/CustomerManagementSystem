a633448fc720ecfa6b5b7a95d25537b3ddae2360
MarlineKhavele <marlinekhavele6@gmail.com>
2020-03-07 22:16:18 +0300

    Added postgres db

--- crm/settings.py
+++ crm/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = "crm.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "accounts",
+        "USER": "postgres",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 # Password validation
