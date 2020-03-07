1ff14a484beede0095dac656e63f33602273cc84
MarlineKhavele <marlinekhavele6@gmail.com>
2020-03-07 20:25:53 +0300

    Added signals to customers

--- accounts/views.py
+++ accounts/views.py
@@ -27,11 +27,7 @@ def registerPage(request):
 			username = form.cleaned_data.get('username')
 
 			group = Group.objects.get(name='customer')
-			user.groups.add(group)
-			Customer.objects.create(
-				user=user,
-				name=user.username,
-				)
+			
 
 			messages.success(request, 'Account was created for ' + username)
 
