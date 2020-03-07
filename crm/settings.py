3f6d8072df990f88e7effa651f8644e137a98a7b
MarlineKhavele <marlinekhavele6@gmail.com>
2020-03-08 00:04:41 +0300

    changes

--- crm/settings.py
+++ crm/settings.py
@@ -124,7 +124,7 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 MEDIA_URL = "/images/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
